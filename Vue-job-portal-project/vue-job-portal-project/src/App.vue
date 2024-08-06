82915ae2a70f9a72e6a547e9937df7aaae14726b
Ayesha Seemab <ayeshaseemab2017@gmail.com>
2024-08-06 11:36:31 +0300

    Display job adds

--- Vue-job-portal-project/vue-job-portal-project/src/App.vue
+++ Vue-job-portal-project/vue-job-portal-project/src/App.vue
@@ -2,9 +2,11 @@
 import NavBar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
 import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
 </script>
 <template>
   <NavBar />
   <Hero title="Test Title" />
   <HomeCards />
+  <JobListings />
 </template>
