e65f6b8a52c3c526dee6d3d31282af1681f0edfb
Ayesha Seemab <ayeshaseemab2017@gmail.com>
2024-08-06 13:28:44 +0300

    Set up router

--- Vue-job-portal-project/vue-job-portal-project/src/App.vue
+++ Vue-job-portal-project/vue-job-portal-project/src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
 import NavBar from "@/components/Navbar.vue";
-import Hero from "@/components/Hero.vue";
-import HomeCards from "@/components/HomeCards.vue";
-import JobListings from "@/components/JobListings.vue";
+import { RouterView } from "vue-router";
 </script>
 <template>
   <NavBar />
-  <Hero title="Test Title" />
-  <HomeCards />
-  <JobListings :limit="4" showButton="true" />
+  <RouterView />
 </template>
